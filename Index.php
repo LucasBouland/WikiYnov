7d53a97838e0145f579d157936b45f2f0fe4557c
Schmurf <lucas.bouland@ynov.com>
2016-05-06 09:23:18 +0200

    Index fix

--- Index.php
+++ Index.php
@@ -13,25 +13,3 @@ if(isset($user)) {
 
 ?>
 
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<div>
-    <form method="post" action="">
-        <input type="pseudo" required name="mail" class="form-control input-sm chat-input"
-               placeholder="Adresse mail"/>
-        <input type="password" required name="mdp" class="form-control input-sm chat-input"
-               placeholder="Password"/>
-        <br>
-        <button type="submit" >Connexion</button>
-    </form>
-</div>
-</body>
-</html>
-=======
->>>>>>> refs/remotes/origin/Pierre
